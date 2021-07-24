c03e5a956ccbc52556a344265c9a57c642045349
unknown <landon.m.holland@gmail.com>
2021-07-24 12:49:11 -0600

    fixed bugs and added error messages

--- app/src/main/java/com/birds/myapplication/MainActivity.java
+++ app/src/main/java/com/birds/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.birds.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     EditText mHeightField;
     EditText mWeightField;
     TextView mBMIView;
+    TextView mErrorField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +26,32 @@ public class MainActivity extends AppCompatActivity {
         mHeightField = ((EditText)findViewById(R.id.heightField));
         mWeightField = ((EditText)findViewById(R.id.weightField));
         mBMIView = ((TextView)findViewById(R.id.BMIView));
+        mErrorField = ((TextView)findViewById(R.id.ErrorField));
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
-                int height = Integer.parseInt(String.valueOf(mHeightField.getText()));
-                int weight = Integer.parseInt(String.valueOf(mWeightField.getText()));
-                double result = (weight / (Math.pow(height, 2)) * 703);
+                if(TextUtils.isEmpty(mHeightField.getText().toString())){
 
-                String stringResult = String.format("%.2f", result);
-                mBMIView.setText(stringResult);
+                    mErrorField.setText("You have not entered your height!");
+                    mBMIView.setText("");
+
+                } else if (TextUtils.isEmpty(mWeightField.getText().toString())){
+
+                    mErrorField.setText("You have not entered your weight!");
+                    mBMIView.setText("");
+
+                } else {
+                    int height = Integer.parseInt(String.valueOf(mHeightField.getText()));
+                    int weight = Integer.parseInt(String.valueOf(mWeightField.getText()));
+                    double result = (weight / (Math.pow(height, 2)) * 703);
+
+                    String stringResult = String.format("%.2f", result);
+                    mBMIView.setText(stringResult);
+
+                    mErrorField.setText("");
+                }
 
             }
         });
